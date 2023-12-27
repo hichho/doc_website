4a4b8521173d135d1546caed7e165969bb13839b
hichho <52339006+hichho@users.noreply.github.com>
2023-12-27 15:47:35 +0800

    test: doc.sh

--- doc.sh
+++ doc.sh
@@ -7,36 +7,28 @@ script_directory="/var/project/doc_website/script"
 initial_timestamp=$(date +%s)
 
 # 监听文件变化并执行拷贝和脚本
-( /usr/bin/inotifywait -m -r -e create,moved_to "$source_directory" &
-  PID=$!
-
-  # 等待一段时间
-  sleep 10
+while true; do
+  # 等待文件变化
+  /usr/bin/inotifywait -q -e create -r "$source_directory"
 
   # 检查文件是否在最后10秒内有新的改动
-  while true; do
-    current_timestamp=$(date +%s)
-    difference=$((current_timestamp - initial_timestamp))
+  current_timestamp=$(date +%s)
+  difference=$((current_timestamp - initial_timestamp))
 
-    if [ $difference -ge 10 ]; then
-        # 输出目录内容以便调试
-        ls -la "$source_directory/"
+  if [ $difference -ge 10 ]; then
+    # 输出目录内容以便调试
+    ls -la "$source_directory/"
 
-        # 执行拷贝操作
-        cp -r "$source_directory"/* "$destination_directory/"
-        echo "Files copied to $destination_directory"
+    # 执行拷贝操作
+    cp -r "$source_directory"/* "$destination_directory/"
+    echo "Files copied to $destination_directory"
 
-        # 执行脚本操作
-        cd "$script_directory" || exit
-        node index.js
-        cd - || exit
-        break
-    fi
+    # 执行脚本操作
+    cd "$script_directory" || exit
+    node index.js
+    cd - || exit
 
-    # 等待一小段时间后再次检查文件
-    sleep 5
-  done
-
-  # 等待inotifywait结束
-  wait $PID
-)
+    # 更新时间戳
+    initial_timestamp=$(date +%s)
+  fi
+done
