b2cf9d364d6fde579b47f7f966cf86319a189258
hichho <52339006+hichho@users.noreply.github.com>
2023-12-27 17:38:26 +0800

    test: doc.sh

--- doc.sh
+++ doc.sh
@@ -12,14 +12,22 @@ while true; do
   /usr/bin/inotifywait -q -e create -e delete -e move -r "$source_directory"
 
   # 同步源目录到目标目录（包括子目录和文件）
-  rsync -r -u "$source_directory/" "$destination_directory/"
+  rsync -av --delete "$source_directory/" "$destination_directory/"
   echo "Files synchronized from $source_directory to $destination_directory"
 
-  # 执行脚本操作
-  cd "$script_directory" || exit
-  node index.js
-  cd - || exit
+  # 获取同步结束时间戳
+  sync_end_timestamp=$(date +%s)
 
-  # 更新时间戳
-  initial_timestamp=$(date +%s)
+  # 检查是否超过一定时间，确保所有文件都已经同步完成
+  time_difference=$((sync_end_timestamp - initial_timestamp))
+  
+  if [ $time_difference -ge 5 ]; then
+    # 执行脚本操作
+    cd "$script_directory" || exit
+    node index.js
+    cd - || exit
+
+    # 重置时间戳
+    initial_timestamp=$(date +%s)
+  fi
 done
