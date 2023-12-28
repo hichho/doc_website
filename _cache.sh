73fff2e1c0beacf7c480101f2ae1fdde8d301947
hichho <52339006+hichho@users.noreply.github.com>
2023-12-28 10:30:46 +0800

    test: script

--- _cache.sh
+++ _cache.sh
@@ -6,35 +6,41 @@ script_directory="/var/project/doc_website/script"
 # 设置初始时间戳
 initial_timestamp=$(date +%s)
 
-# 同步文件夹并监听文件变化
+# 创建目标目录（如果不存在）
+mkdir -p "$destination_directory"
+
+# 同步文件夹并持续监听文件变化
 while true; do
-  # 等待文件变化
-  /usr/bin/inotifywait -q -e create -e delete -e move -r "$source_directory"
+  # 检测文件变化
+  /usr/bin/inotifywait -r -qq -e create -e delete -e move "$source_directory"
 
-  # 同步源目录到目标目录（覆盖目标目录）
-  rsync -av --delete "$source_directory/" "$destination_directory/"
-  echo "Files synchronized from $source_directory to $destination_directory"
+  # 删除目标目录下所有文件
+  rm -rf "$destination_directory"/*
 
-  # 检查源目录是否完全同步到目标目录
-  diff_output=$(diff -qr "$source_directory" "$destination_directory")
+  # 等待上传完成
+  sleep 20
 
-  # 如果源目录与目标目录内容相同
-  if [ -z "$diff_output" ]; then
-    # 记录同步完成时间戳
-    sync_end_timestamp=$(date +%s)
+  # 复制源目录到目标目录
+  cp -R "$source_directory/." "$destination_directory/"
+  echo "Files copied from $source_directory to $destination_directory"
 
-    # 检查同步完成后是否经过一段时间，用于确保所有文件已复制
-    time_difference=$((sync_end_timestamp - initial_timestamp))
+  # 检查源目录是否与目标目录内容相同
+  if diff -qr "$source_directory" "$destination_directory" > /dev/null; then
+    # 等待一段时间以确保所有文件都已复制（可根据需要调整等待时间）
+    sleep 10
 
-    # 如果已经经过一段时间（比如5秒），则执行脚本
-    if [ $time_difference -ge 5 ]; then
+    # 检查目标目录是否有新的文件添加
+    new_files=$(diff -qr "$source_directory" "$destination_directory" | grep "Only in $source_directory")
+
+    # 如果没有新文件添加，执行脚本操作
+    if [ -z "$new_files" ]; then
       # 执行脚本操作
       cd "$script_directory" || exit
       node index.js
       cd - || exit
 
-      # 结束循环，任务完成
-      break
+      # 继续监听文件变化
+      continue
     fi
   fi
 done
