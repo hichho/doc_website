b0ef5b9b6994142290b0b40c307d03370952d28a
hichho <52339006+hichho@users.noreply.github.com>
2023-12-28 13:12:13 +0800

    test: script

--- doc.sh
+++ doc.sh
@@ -6,25 +6,25 @@ script_directory="/var/project/doc_website/script"
 # 设置初始时间戳
 initial_timestamp=$(date +%s)
 
-# 同步文件夹并监听文件变化
-while true; do
-  # 等待文件变化
-  /usr/bin/inotifywait -q -e create -e delete -e move -r "$source_directory"
+# 创建目标目录（如果不存在）
+mkdir -p "$destination_directory"
 
+# 同步文件夹并持续监听文件变化的函数
+sync_and_listen() {
   # 删除目标目录下所有文件
   rm -rf "$destination_directory"/*
+
+  # 等待上传完成
+  sleep 20
 
   # 复制源目录到目标目录
   cp -R "$source_directory/." "$destination_directory/"
   echo "Files copied from $source_directory to $destination_directory"
 
-  # 检查源目录是否与目标目录相同
-  diff_output=$(diff -qr "$source_directory" "$destination_directory")
-
-  # 如果源目录与目标目录内容相同
-  if [ -z "$diff_output" ]; then
+  # 检查源目录是否与目标目录内容相同
+  if diff -qr "$source_directory" "$destination_directory" > /dev/null; then
     # 等待一段时间以确保所有文件都已复制（可根据需要调整等待时间）
-    sleep 5
+    sleep 10
 
     # 检查目标目录是否有新的文件添加
     new_files=$(diff -qr "$source_directory" "$destination_directory" | grep "Only in $source_directory")
@@ -36,8 +36,11 @@ while true; do
       node index.js
       cd - || exit
 
-      # 结束循环，任务完成
-      break
+      # 继续监听文件变化
+      sync_and_listen
     fi
   fi
-done
+}
+
+# 初始执行
+sync_and_listen
